a14ffe41e271f565020744d59da2955219602789
Pedro Enciso <penciso@ucsd.edu>
2024-06-17 11:07:06 -0700

    updated styles and added hook to send a POST request

--- src/components/NewHighScore/index.jsx
+++ src/components/NewHighScore/index.jsx
@@ -4,11 +4,12 @@ import style from "./NewHighScore.module.css";
 
 function NewHighScore({ onSubmit, place = "4th" }) {
   const name = useRef();
+  // const {data, error, isLoading, submitHighScore} = usePostRequest();
 
   function submit(e) {
     e.preventDefault();
-    // http request to submit highscore
-    console.log("submit");
+
+    // submitHighScore(place, score, name)
     onSubmit();
   }
 
@@ -17,15 +18,24 @@ function NewHighScore({ onSubmit, place = "4th" }) {
       {createPortal(
         <div className={style.container}>
           <form onSubmit={submit} className={style.form}>
-            <p className={style.title}>NEW HIGH SCORE!</p>
-            <p className={style.place}>{place}</p>
-            <input
-              type="text"
-              ref={name}
-              className={style.input}
-              maxLength="4"
-            />
-            <button className="btn blink">Submit</button>
+            <div>
+              <p className={style.title}>NEW HIGH SCORE!</p>
+              <p className={style.score}>100000</p>
+            </div>
+            <div>
+              <p className={style.text}>You placed</p>
+              <p className={style.place}>{place}</p>
+            </div>
+            <div>
+              <input
+                type="text"
+                ref={name}
+                className={style.input}
+                maxLength="4"
+              />
+              <p className={`${style.text} ${style.mt}`}>Enter your name</p>
+            </div>
+            <button className={`${style.btn_shadow} btn blink`}>SUBMIT</button>
           </form>
         </div>,
         document.body
@@ -33,5 +43,30 @@ function NewHighScore({ onSubmit, place = "4th" }) {
     </>
   );
 }
+
+const usePostRequest = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [data, setData] = useState(null);
+
+  async function submitHighScore() {
+    setIsLoading(true);
+    try {
+      ///// CHANGE URL
+      const response = await fetch("url");
+
+      if (!response.ok) {
+        throw new Error("Something went wrong");
+      }
+
+      setData(await response.json());
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  }
+
+  return { data, error, isLoading, submitHighScore };
+};
 
 export default NewHighScore;
