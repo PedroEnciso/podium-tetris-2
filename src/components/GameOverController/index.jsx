36dfc95030089af59be65df715cb37c0fe88bc8e
Pedro Enciso <penciso@ucsd.edu>
2024-06-18 13:56:19 -0700

    update props

--- src/components/GameOverController/index.jsx
+++ src/components/GameOverController/index.jsx
@@ -2,18 +2,20 @@ import React, { useEffect, useState } from "react";
 import GameOver from "../GameOver";
 import Leaderboard from "../Leaderboard";
 import NewHighScore from "../NewHighScore";
+import { useUserContext } from "../../context/user-context";
 
 function GameOverController({ restartClick, status }) {
   // can be 'game over' | 'new high score' | 'leaderboard'
   const [currentScreen, setCurrentScreen] = useState("game over");
   // const { data, error, fetchPlace } = useFetch();
+  const { user } = useUserContext();
 
   function showLeaderboard() {
     setCurrentScreen("leaderboard");
   }
 
   if (currentScreen === "game over") {
-    // fetchPlace()
+    // fetchPlace() to check if score is a new high score
     setTimeout(() => {
       setCurrentScreen("new high score");
     }, 2000);
@@ -22,7 +24,7 @@ function GameOverController({ restartClick, status }) {
 
   if (currentScreen === "new high score") {
     console.log("new high score");
-    return <NewHighScore onSubmit={showLeaderboard} />;
+    return <NewHighScore onSubmit={showLeaderboard} user={user} />;
   }
 
   if (currentScreen === "leaderboard") {
