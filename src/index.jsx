7e5a6001966533eb565e7b58cc530d53f3b5a204
Pedro Enciso <penciso@ucsd.edu>
2024-06-24 14:42:38 -0700

    install msw

--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,11 @@ import "./index.css";
 
 import App from "./App";
 
+import { worker } from "./mocks/browser";
+if (process.env.NODE_ENV === "development") {
+  worker.start();
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
