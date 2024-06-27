f9f9fb2504a5187bc479ae32a21be18d60f66f98
Pedro Enciso <penciso@ucsd.edu>
2024-06-27 12:38:47 -0700

    responsive styles

--- src/components/Stage/index.jsx
+++ src/components/Stage/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import styled from "styled-components";
 import Switch from "react-switch";
+import style from "./Stage.module.css";
 
 import useWindowDimensions from "../../hooks/useWindowDimensions";
 import background from "../../images/background.jpg";
@@ -16,78 +17,41 @@ const Game = styled.div`
   /* height: 100vh; */
   height: 100svh;
   overflow: hidden;
+  padding: 2rem 0rem;
   display: flex;
-  flex-direction: ${(props) => (props.$portrait ? "column" : "row")};
-  flex-wrap: ${(props) => (props.$portrait ? "nowrap" : "wrap")};
   justify-content: center;
   align-items: center;
-  /* background-image: url(${background}); */
-  background-size: cover;
-  background-position: center;
 `;
 
-const ContainerNext = styled.div`
-  ${(props) =>
-    !props.$portrait && `height: ${props.$pixelSize * 18 + (18 / 3) * 1}px;`}
-  ${(props) =>
-    props.$portrait && `width: ${props.$pixelSize * 10 + (10 / 3) * 1}px;`}
-	margin-right: ${(props) => (props.$portrait ? 0 : props.$pixelSize / 3)}px;
-  margin-bottom: ${(props) => (props.$portrait ? props.$pixelSize / 3 : 0)}px;
-  display: flex;
-  flex-direction: ${(props) => (props.$portrait ? "row" : "column")};
-  justify-content: ${(props) =>
-    props.$portrait ? "space-between" : "flex-start"};
-  align-items: center;
-`;
+const ContainerNext = styled.div``;
 
 const Next = styled.div`
-  width: ${(props) => props.$pixelSize * 3}px;
-  height: ${(props) => props.$pixelSize * 3}px;
-  background-color: ${(props) => (props.$theme3d ? "#444" : "black")};
+  /* width: ${(props) => props.$pixelSize * 3}px;
+  height: ${(props) => props.$pixelSize * 3}px; */
   transition: background-color 0.5;
-  border: ${(props) => props.$pixelSize / 10}px solid white;
   overflow: hidden;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  margin-bottom: ${(props) => (props.$portrait ? 0 : props.$pixelSize / 3)}px;
-  margin-right: ${(props) => (!props.$portrait ? 0 : props.$pixelSize / 3)}px;
-`;
-
-const StyledStage = styled.div`
-  border: ${(props) => props.$pixelSize / 10}px solid white;
-  background-color: ${(props) => (props.$theme3d ? "#444" : "black")};
-  transition: background-color 0.5s;
-  overflow: hidden;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  position: relative;
-`;
-
-const Center = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  /* margin-bottom: ${(props) =>
+    props.$portrait ? 0 : props.$pixelSize / 3}px;
+  margin-right: ${(props) =>
+    !props.$portrait ? 0 : props.$pixelSize / 3}px; */
 `;
 
 const Row = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: center;
-  height: ${(props) =>
-    props.$stage ? props.$pixelSize : props.$pixelSize / 1.6}px;
+  /* height: 20px; */
+  /* height: ${(props) =>
+    props.$stage ? props.$pixelSize : props.$pixelSize / 1.6}px; */
 `;
 
 const Pixel = React.memo(styled.div`
-  width: ${(props) =>
-    props.$stage ? props.$pixelSize : props.$pixelSize / 1.6}px;
-  height: ${(props) =>
-    props.$stage ? props.$pixelSize : props.$pixelSize / 1.6}px;
+  /* height: clamp(25px, calc(80% / 10), 45px);
+  width: clamp(25px, calc(80% / 10), 45px); */
   background-color: ${(props) =>
     props.$fill === 1 ? props.$color : "inherited"};
   position: relative;
@@ -103,11 +67,9 @@ const Pixel = React.memo(styled.div`
     props.$fill &&
     props.$theme3d &&
     `;
-		box-shadow: ${props.$pixelSize / 4.16}px ${props.$pixelSize / 4.16}px ${
-      props.pixelSize / 5.55
-    }px #222${
+    box-shadow: calc(clamp(25px, calc(80% / 10), 45px) / 4.16) calc(clamp(25px, calc(80% / 10), 45px) / 4.16) calc(clamp(25px, calc(80% / 10), 45px) / 5.55) #222${
       props.$topBloco
-        ? `, 0 ${-props.$pixelSize / 4.16}px 0 ${Color(props.$color).lighten(
+        ? `, 0 calc(clamp(25px, calc(80% / 10) 0 ${Color(props.$color).lighten(
             0.2
           )}`
         : ""
@@ -133,41 +95,17 @@ const Pixel = React.memo(styled.div`
 	`};
 `);
 
-const ContainerSwitch = styled.div`
-  ${(props) =>
-    props.$portrait &&
-    `
-		height: 100%;
-		width: 100%;
-		display: flex;
-		align-items: flex-end;
-		flex-direction: column;
-		justify-content: flex-end;
-	`};
-`;
-
 const ContainerStatus = styled.div`
-  width: ${(props) => props.$pixelSize * 8}px;
-  ${(props) =>
+  /* ${(props) =>
     !props.$portrait && `height: ${props.$pixelSize * 18 + (18 / 3) * 1}px;`}
   ${(props) =>
     props.$portrait && `width: ${props.$pixelSize * 10 + (10 / 3) * 1}px;`}
-	margin-left: ${(props) => (props.$portrait ? 0 : props.$pixelSize / 3)}px;
-  margin-top: ${(props) => (props.$portrait ? props.$pixelSize / 3 : 0)}px;
   display: flex;
   flex-direction: ${(props) => (props.$portrait ? "row" : "column")};
   align-items: center;
   justify-content: ${(props) =>
     props.$portrait ? "space-between" : "flex-start"};
-  font-size: ${(props) => props.$pixelSize}px;
-`;
-
-const ContainerGameMode = styled.div`
-  width: 100%;
-  text-align: center;
-  color: white;
-  font-size: 1.5rem;
-  text-transform: capitalize;
+  font-size: ${(props) => props.$pixelSize}px; */
 `;
 
 const getRenderizacaoBloco = (bloco) => {
@@ -243,156 +181,149 @@ const Stage = ({
   const { gameMode } = useGameMode();
 
   return (
-    <div>
-      <Game $portrait={portrait}>
-        <ContainerGameMode>
-          <p>{gameMode}</p>
-        </ContainerGameMode>
-        {nextRender && (
-          <ContainerNext $portrait={portrait} $pixelSize={pixelSize}>
-            <Next
+    <>
+      <Game $portrait={portrait} className={style.game_container}>
+        <div className={style.game_inside_container}>
+          {nextRender && (
+            <ContainerNext
+              className={style.container_next}
               $portrait={portrait}
-              $theme3d={theme3d}
               $pixelSize={pixelSize}
             >
-              {nextRender.map((row, y) => (
-                <Row $pixelSize={pixelSize} key={`row-${y}`}>
-                  {row.map((pixel, x) => {
-                    let topBloco =
-                      pixel && (!nextRender[y - 1] || !nextRender[y - 1][x]);
-                    return (
-                      <Pixel
-                        $paused={paused}
-                        $theme3d={theme3d}
-                        $topBloco={topBloco}
-                        $zIndex={y}
-                        $pixelSize={pixelSize}
-                        key={`pixel-${x}`}
-                        $fill={pixel}
-                        $color={player.next.color}
-                      />
-                    );
-                  })}
-                </Row>
-              ))}
-            </Next>
-            <ContainerSwitch $portrait={portrait}>
-              <Switch
-                width={pixelSize * 2}
-                height={pixelSize / 1.2}
-                onChange={setTheme3d}
-                checked={theme3d}
-                offColor="#000"
-                onColor="#444"
-                uncheckedIcon={
-                  <Center>
-                    <Pixel
-                      $theme3d={false}
-                      $pixelSize={pixelSize / 3}
-                      $stage="true"
-                      $fill={1}
-                      $color={"#e54b4b"}
-                    />
-                  </Center>
-                }
-                checkedIcon={
-                  <Center>
-                    <Pixel
-                      $theme3d="true"
-                      $pixelSize={pixelSize / 3}
-                      $stage="true"
-                      $fill={1}
-                      $color={"#e54b4b"}
-                      $topBloco="true"
-                    />
-                  </Center>
-                }
+              <Next
+                $portrait={portrait}
+                $theme3d={theme3d}
+                $pixelSize={pixelSize}
+              >
+                {nextRender.map((row, y) => (
+                  <Row
+                    className={style.next_row}
+                    $pixelSize={pixelSize}
+                    key={`row-${y}`}
+                  >
+                    {row.map((pixel, x) => {
+                      let topBloco =
+                        pixel && (!nextRender[y - 1] || !nextRender[y - 1][x]);
+                      return (
+                        <Pixel
+                          className={style.next_pixel}
+                          $paused={paused}
+                          $theme3d={theme3d}
+                          $topBloco={topBloco}
+                          $zIndex={y}
+                          $pixelSize={pixelSize}
+                          key={`pixel-${x}`}
+                          $fill={pixel}
+                          $color={player.next.color}
+                        />
+                      );
+                    })}
+                  </Row>
+                ))}
+              </Next>
+              <div
+                className={`${style.mode_container} ${style.small_container}`}
+              >
+                <p className={style.mode_title}>MODE</p>
+                <p className={style.mode}>{gameMode}</p>
+              </div>
+            </ContainerNext>
+          )}
+          <div class={style.stage_mode_container}>
+            <div className={`${style.mode_container} ${style.large_container}`}>
+              <p className={style.mode_title}>MODE</p>
+              <p className={style.mode}>{gameMode}</p>
+            </div>
+            {map && (
+              <div
+                className={style.stage}
+                ref={stageRef}
+                {...others}
+                id="stage"
+              >
+                {map.map((row, y) => (
+                  <Row $stage="true" $pixelSize={pixelSize} key={`row-${y}`}>
+                    {row.map((pixel, x) => {
+                      let playerFill =
+                        player.bloco.bloco[y - player.pos[0]] &&
+                        player.bloco.bloco[y - player.pos[0]][
+                          x - player.pos[1]
+                        ];
+                      let playerHint =
+                        hint.bloco.bloco[y - hint.pos[0]] &&
+                        hint.bloco.bloco[y - hint.pos[0]][x - hint.pos[1]];
+                      let topBloco =
+                        (playerFill || pixel.fill) &&
+                        (!player.bloco.bloco[y - player.pos[0] - 1] ||
+                          !player.bloco.bloco[y - player.pos[0] - 1][
+                            x - player.pos[1]
+                          ]) &&
+                        (!map[y - 1] || !map[y - 1][x].fill);
+                      let zIndex =
+                        !playerFill && !pixel.fill && playerHint ? 99 : y;
+                      return (
+                        <Pixel
+                          className={style.pixel}
+                          $paused={paused}
+                          $theme3d={theme3d}
+                          $hint={!pixel.fill && !playerFill && playerHint}
+                          $pixelSize={pixelSize}
+                          $stage="true"
+                          key={`pixel-${x}`}
+                          $fill={pixel.fill || playerFill}
+                          $color={playerFill ? player.bloco.color : pixel.color}
+                          $playerColor={player.bloco.color}
+                          $topBloco={topBloco}
+                          $zIndex={zIndex}
+                        ></Pixel>
+                      );
+                    })}
+                  </Row>
+                ))}
+              </div>
+            )}
+          </div>
+          {status && (
+            <ContainerStatus
+              className={style.status_container}
+              $portrait={portrait}
+              $pixelSize={pixelSize}
+            >
+              <StatusRow
+                backgroundColor={theme3d ? "#444" : "black"}
+                portrait={portrait}
+                borderSize={pixelSize / 10}
+                margin={pixelSize / 3}
+                padding={pixelSize / 2}
+                title="SCORE"
+                value={status.score}
               />
-            </ContainerSwitch>
-          </ContainerNext>
-        )}
-        {map && (
-          <StyledStage
-            ref={stageRef}
-            {...others}
-            $theme3d={theme3d}
-            $pixelSize={pixelSize}
-            id="stage"
-          >
-            {map.map((row, y) => (
-              <Row $stage="true" $pixelSize={pixelSize} key={`row-${y}`}>
-                {row.map((pixel, x) => {
-                  let playerFill =
-                    player.bloco.bloco[y - player.pos[0]] &&
-                    player.bloco.bloco[y - player.pos[0]][x - player.pos[1]];
-                  let playerHint =
-                    hint.bloco.bloco[y - hint.pos[0]] &&
-                    hint.bloco.bloco[y - hint.pos[0]][x - hint.pos[1]];
-                  let topBloco =
-                    (playerFill || pixel.fill) &&
-                    (!player.bloco.bloco[y - player.pos[0] - 1] ||
-                      !player.bloco.bloco[y - player.pos[0] - 1][
-                        x - player.pos[1]
-                      ]) &&
-                    (!map[y - 1] || !map[y - 1][x].fill);
-                  let zIndex =
-                    !playerFill && !pixel.fill && playerHint ? 99 : y;
-                  return (
-                    <Pixel
-                      $paused={paused}
-                      $theme3d={theme3d}
-                      $hint={!pixel.fill && !playerFill && playerHint}
-                      $pixelSize={pixelSize}
-                      $stage="true"
-                      key={`pixel-${x}`}
-                      $fill={pixel.fill || playerFill}
-                      $color={playerFill ? player.bloco.color : pixel.color}
-                      $playerColor={player.bloco.color}
-                      $topBloco={topBloco}
-                      $zIndex={zIndex}
-                    ></Pixel>
-                  );
-                })}
-              </Row>
-            ))}
-          </StyledStage>
-        )}
-        {status && (
-          <ContainerStatus $portrait={portrait} $pixelSize={pixelSize}>
-            <StatusRow
-              backgroundColor={theme3d ? "#444" : "black"}
-              portrait={portrait}
-              borderSize={pixelSize / 10}
-              margin={pixelSize / 3}
-              padding={pixelSize / 2}
-              title="SCORE"
-              value={status.score}
-            />
-            <StatusRow
-              backgroundColor={theme3d ? "#444" : "black"}
-              portrait={portrait}
-              borderSize={pixelSize / 10}
-              margin={pixelSize / 3}
-              padding={pixelSize / 2}
-              title="LEVEL"
-              value={status.level}
-            />
-            <StatusRow
-              backgroundColor={theme3d ? "#444" : "black"}
-              portrait={portrait}
-              borderSize={pixelSize / 10}
-              margin={pixelSize / 3}
-              padding={pixelSize / 2}
-              title="LINES"
-              value={status.lines}
-            />
-          </ContainerStatus>
-        )}
+              <StatusRow
+                backgroundColor={theme3d ? "#444" : "black"}
+                portrait={portrait}
+                borderSize={pixelSize / 10}
+                margin={pixelSize / 3}
+                padding={pixelSize / 2}
+                title="LEVEL"
+                value={status.level}
+              />
+              <StatusRow
+                backgroundColor={theme3d ? "#444" : "black"}
+                portrait={portrait}
+                borderSize={pixelSize / 10}
+                margin={pixelSize / 3}
+                padding={pixelSize / 2}
+                title="LINES"
+                value={status.lines}
+              />
+            </ContainerStatus>
+          )}
+        </div>
       </Game>
       {lose ? (
         <GameOverController restartClick={restartClick} status={status} />
       ) : null}
-    </div>
+    </>
   );
 };
 
--- src/components/StatusRow/index.jsx
+++ src/components/StatusRow/index.jsx
@@ -2,28 +2,21 @@ import React from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
-  width: ${(props) => props.$portrait && "30%"};
-  background-color: ${(props) =>
-    props.$backgroundColor ? props.$backgroundColor : "black"};
   transition: background-color 0.5s;
-  border: ${(props) => (props.$borderSize ? props.$borderSize : "3")}px solid
-    white;
-  ${(props) => !props.$portrait && `width: 100%;`}
-  font-family: "ZCOOL QingKe HuangYou", cursive;
-  padding: ${(props) => (props.$padding ? props.$padding : "15")}px
-    ${(props) => (props.$portrait ? props.$padding / 2 : 0)}px; /*15*/
-  margin-bottom: ${(props) => (props.$margin ? props.$margin : "10")}px; /*10*/
+  padding: 15px;
+  font-family: "VT323", monospace;
 `;
 
 const Title = styled.div`
   width: 100%;
-  font-size: ${(props) => props.$portrait && "1.2rem"};
+  font-size: 1.25rem;
   text-align: center;
   color: white;
 `;
 
 const Value = styled.div`
   width: 100%;
+  font-size: 2rem;
   text-align: center;
   color: white;
 `;
