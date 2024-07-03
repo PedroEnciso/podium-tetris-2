e5495f2c134da578955a5d1b5dbb7aae75fc7691
Pedro Enciso <penciso@ucsd.edu>
2024-07-03 14:01:00 -0700

    mobile fix 06

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,6 @@ import { GameModeProvider } from "./context/game-mode-context";
 import { UserContextProvider } from "./context/user-context";
 import Tetris from "./components/Tetris";
 
-// fix select when dragging on mobile
-document.body.addEventListener("touchstart", (e) => e.preventDefault(), {
-  passive: false,
-});
-
 export default function App() {
   return (
     <UserContextProvider>
